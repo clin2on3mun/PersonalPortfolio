8ac3a0d548ddb7a80c6100081b41699e17177c22
clin20n3mun <munanaclinton@gmail.com>
2023-11-22 19:05:18 +0200

    Create nav bar component and render it in app

---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = ()=>{
+
+    return(
+        <header>
+            <nav>
+                <h1> Logo </h1>
+                <ul>
+                 <li><a href="#Home"></a>Home</li>
+                 <li><a href="#About"></a>About</li>
+                 <li><a href="#projects"></a>Projects</li>
+                 <li><a href="#skills"></a>Skills</li>
+                 <li><a href="#contact"></a>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Navbar
