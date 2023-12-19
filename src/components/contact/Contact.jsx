37326e96308e57638fff4f29bed02b97c639d8d6
clin20n3mun <munanaclinton@gmail.com>
2023-12-19 10:45:00 +0200

    Add change to these files

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,16 +2,18 @@ import './contact.css'
 const Contact = ()=>{
     return (
         <section id="contact">
-                <section className="contact-content">
-                    <h2 className='about-text'>Contact Me</h2>
-                    <p>Let &apos;s get in touch</p>
-                </section>
-                <form id ='form'>
-                    <input type='text' placeholder='Name' className='form-control'/>
-                    <input type='email' placeholder='Email' className='form-control'/>
-                    <textarea placeholder='say something.........' className='form-control text-area'></textarea>
-                    <button type="submit" className="btn-send">Send</button>
-                </form>
+              <section>
+                    <section className="contact-content">
+                        <h2 className='about-text'>Contact Me</h2>
+                        <p>Let &apos;s get in touch</p>
+                    </section>
+                    <form id ='form'>
+                        <input type='text' placeholder='Name' className='form-control'/>
+                        <input type='email' placeholder='Email' className='form-control'/>
+                        <textarea placeholder='say something.........' className='form-control text-area'></textarea>
+                        <button type="submit" className="btn-send">Send</button>
+                    </form>
+            </section>
         </section>
     )
 }
