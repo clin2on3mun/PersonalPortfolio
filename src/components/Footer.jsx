2e9218977d0185bc160f2df30794cf083019ebf8
clin20n3mun <munanaclinton@gmail.com>
2023-12-19 11:10:55 +0200

    Add style to footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,11 @@ import { FaAngellist } from "react-icons/fa";
 
 const Footer =()=>{
     return (
-        <footer className="socials">
+        <footer className="socials" style={{position:"relative"}}>
                 <a href="https://github.com/clin2on3mun" target="_blank" className="social-icons github" rel="noreferrer"><FiGithub /></a>
                 <a href="https://www.linkedin.com/in/munana-clinton/" target="_blank" className="social-icons linkedin" rel="noreferrer"><FaLinkedinIn/></a>
                 <a href="https://wellfound.com/u/munana-clinton" target="_blank" className="social-icons angelist" rel="noreferrer"><FaAngellist /></a>
+                <a style={{position:"absolute", top: "20%", left:"70%", fontSize:"1.25em"}} className="back" href="#home">back to top</a>
         </footer>
     )
 }
