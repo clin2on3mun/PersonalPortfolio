9c8c4c8f5ae722fd94a097e036c72d663213e00c
clin20n3mun <munanaclinton@gmail.com>
2023-12-19 15:43:59 +0200

    Add changes to home component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,16 @@
 import { FiGithub } from "react-icons/fi";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaAngellist } from "react-icons/fa";
+import { motion } from 'framer-motion'
 import './home.css'
 import Myimage from "./landingImage";
 const Home = ()=>{
     return(
         <section id="home">
-            <div className="hero">
+            <motion.div className="hero"
+                        initial={{opacity: 0, y:100}}
+                        whileInView={{opacity:1, y:0}}
+                        transition={{duration:1}}>
                 <h1 className='hero-text'>Hello, I&apos;m Munana 👋.</h1>
                 <p className="hero-text-me">I&apos;m a Full-Stack Developer</p>
                 <p className="hero-about">Crafting seamless user experiences. Passionate about open-source contributions and dedicated to the art of UI/UX. Let &apos;s build something amazing together!</p>
@@ -15,10 +19,13 @@ const Home = ()=>{
                   <a href="https://www.linkedin.com/in/munana-clinton/" target="_blank" className="social-icons linkedin" rel="noreferrer"><FaLinkedinIn/></a>
                   <a href="https://wellfound.com/u/munana-clinton" target="_blank" className="social-icons angelist" rel="noreferrer"><FaAngellist /></a>
                 </div>
-            </div>
-            <div className="image-home-page">
+            </motion.div>
+            <motion.div className="image-home-page"
+                        initial={{opacity: 0, y:-100}}
+                        whileInView={{opacity:1, y:0}}
+                        transition={{duration:1}}>
                 <Myimage/>
-            </div>
+            </motion.div>
         </section>
     )
 }
