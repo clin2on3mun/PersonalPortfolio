f746a201b4d09c6b49790daa26ba826b99526960
clin20n3mun <munanaclinton@gmail.com>
2023-12-19 12:12:42 +0200

    Add changes to contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,15 @@
+import { useState } from 'react';
 import './contact.css'
 const Contact = ()=>{
+    const [buttonState, setButtonState] = useState("Send")
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const handleSubmit = (event) => {
+        if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+            event.preventDefault();
+          }
+      };
     return (
         <section id="contact">
               <section>
@@ -7,11 +17,11 @@ const Contact = ()=>{
                         <h2 className='about-text'>Contact Me</h2>
                         <p>Let &apos;s get in touch</p>
                     </section>
-                    <form id ='form'>
-                        <input type='text' placeholder='Name' className='form-control'/>
-                        <input type='email' placeholder='Email' className='form-control'/>
-                        <textarea placeholder='say something.........' className='form-control text-area'></textarea>
-                        <button type="submit" className="btn-send">Send</button>
+                    <form id ='form' onSubmit={handleSubmit} action="https://formspree.io/f/xvonwpvy" method="POST">
+                        <input type='text' placeholder='Name' name="name" onChange={(e) => setName(e.target.value)}  className='form-control' required/>
+                        <input type='email' placeholder='Email'name="email" onChange={(e) => setEmail(e.target.value)} className='form-control' required/>
+                        <textarea placeholder='say something.........' name="message"  onChange={(e) => setMessage(e.target.value)} className='form-control text-area' required></textarea>
+                        <button type="submit" className="btn-send">{buttonState}</button>
                     </form>
             </section>
         </section>
